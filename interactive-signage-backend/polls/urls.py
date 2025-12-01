dbb516ba93a4a89e74acd90eb1bf3baaa29fac21
Saketh Rosanuru <142926878+sakethsai1@users.noreply.github.com>
2025-12-01 00:40:10 -0600

    Update urls.py
    added url paths
    
    api/receive-button/ 
    
    //SSE
    api/event/

--- interactive-signage-backend/polls/urls.py
+++ interactive-signage-backend/polls/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("showinfo/", views.showinfo, name="showinfo"),
     path("receive-id/", views.receive_den_id, name="receive_den_id"),
-    path("resetinfo/", views.restartflag, name="restartflag"),
+    path("resetinfo/", views.restartflag, name="restartflag"),  
+    path("receive-button/", views.receive_button_press, name="receive_button_press"),
+    path("events/", views.sse_events, name="sse_events"),  # SSE endpoint
 ]
